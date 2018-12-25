1a97a51a89db08426ce75a2384bfb2839cabdde2
alectramell <alectramell@gmail.com>
2018-12-24 20:11:26 -0800

    ionic

--- usr/share/applications/ioniclib/index.php
+++ usr/share/applications/ioniclib/index.php
@@ -1,6 +1,6 @@
 <?php
 	$xtitle = 'IONIC v1.0';
-	$xopt1 = 'BUILD';
+	$xopt1 = 'NEW';
 	$xopt2 = 'LOAD';
 	$xopt3 = 'EXIT';
 	$xrandom = rand(1, 2);
@@ -33,11 +33,32 @@
 
 <font id="opt3Text" onmouseover="this.style.color = '#ffffff';this.style.left = '85px';this.style.background = '#270000';" onmouseout="this.style.color = '#ffffff';this.style.left = '35px';this.style.background = '#181818';" onclick="closeApp();"><?php echo $xopt3; ?></font>
 
+<!-- in-menu title text -->
+
+<font id="geneTitle" style="display:none;">Select Your Family Gene</font>
+<font id="geneSteps" style="display:none;">Profile Setup 1 of 3</font>
+
 <!-- in-menu icons buttons.. -->
 
-<img id="geneA" src="img/geneA.png" title="The Poseidon Gene" onmouseover="this.style = 'position:absolute;top:35%;left:20%;width:139px;height:139px;border:5px solid #0048ff;border-radius: 20px;background:#000000;padding:5px 7px;opacity:1.0;z-index:5;';" onmouseout="this.style = 'position:absolute;top:35%;left:20%;width:139px;height:139px;border:5px solid #4c4c4c;border-radius: 20px;background:#000000;padding:5px 7px;opacity:1.0;z-index:5;';" style="display:none;">
-<img id="geneB" src="img/geneB.png" title="The Gaia Gene" onmouseover="this.style = 'position:absolute;top:35%;left:40%;width:139px;height:139px;border:5px solid #7eff00;border-radius: 20px;background:#000000;padding:5px 7px;opacity:1.0;z-index:5;';" onmouseout="this.style = 'position:absolute;top:35%;left:40%;width:139px;height:139px;border:5px solid #4c4c4c;border-radius: 20px;background:#000000;padding:5px 7px;opacity:1.0;z-index:5;';" style="display:none;">
-<img id="geneC" src="img/geneC.png" title="The Hedes Gene" onmouseover="this.style = 'position:absolute;top:35%;left:60%;width:139px;height:139px;border:5px solid #ff0f0f;border-radius: 20px;background:#000000;padding:5px 7px;opacity:1.0;z-index:5;';" onmouseout="this.style = 'position:absolute;top:35%;left:60%;width:139px;height:139px;border:5px solid #4c4c4c;border-radius: 20px;background:#000000;padding:5px 7px;opacity:1.0;z-index:5;';" style="display:none;">
+<div id="genePool">
+<img id="geneA" src="img/geneA.png" title="The Poseidon Gene" onmouseover="this.style = 'width:139px;height:139px;border:5px solid #0048ff;border-radius: 20px;background:#000000;padding:5px 7px;z-index:4;';" onmouseout="this.style = 'width:139px;height:139px;border:5px solid #4c4c4c;border-radius: 20px;background:#000000;padding:5px 7px;z-index:4;';" onclick="stepTwoGeneA();hideAllGenes();" style="display:none;">
+<img id="geneB" src="img/geneB.png" title="The Gaia Gene" onmouseover="this.style = 'width:139px;height:139px;border:5px solid #7eff00;border-radius: 20px;background:#000000;padding:5px 7px;z-index:4;';" onmouseout="this.style = 'width:139px;height:139px;border:5px solid #4c4c4c;border-radius: 20px;background:#000000;padding:5px 7px;z-index:4;';" onclick="stepTwoGeneB();hideAllGenes();" style="display:none;">
+<img id="geneC" src="img/geneC.png" title="The Hedes Gene" onmouseover="this.style = 'width:139px;height:139px;border:5px solid #ff0f0f;border-radius: 20px;background:#000000;padding:5px 7px;z-index:4;';" onmouseout="this.style = 'width:139px;height:139px;border:5px solid #4c4c4c;border-radius: 20px;background:#000000;padding:5px 7px;z-index:4;';" onclick="stepTwoGeneC();hideAllGenes();" style="display:none;">
+</div>
+
+<!-- gender-menu opetions -->
+
+<div id="geneXY">
+<img id="femaleOpt" src="img/female.png" style="display:none;" onmouseover="this.style = 'position:absolute;top:35%;left:50%;width:139px;height:139px;border:5px solid #ff4a80;border-radius: 20px;background:#000000;padding:5px 7px;opacity:1.0;z-index:5;';" onmouseout="this.style = 'position:absolute;top:35%;left:50%;width:139px;height:139px;border:5px solid #4c4c4c;border-radius: 20px;background:#000000;padding:5px 7px;opacity:1.0;z-index:5;';" onclick="openUserAttsFemale();">
+<img id="maleOpt" src="img/male.png" style="display:none;" onmouseover="this.style = 'position:absolute;top:35%;left:30%;width:139px;height:139px;border:5px solid #30aefe;border-radius: 20px;background:#000000;padding:5px 7px;opacity:1.0;z-index:5;';" onmouseout="this.style = 'position:absolute;top:35%;left:30%;width:139px;height:139px;border:5px solid #4c4c4c;border-radius: 20px;background:#000000;padding:5px 7px;opacity:1.0;z-index:5;';" onclick="openUserAttsMale();">
+</div>
+
+<div id="userAtts" style="display:none;">
+<input id="newUserName" type="text" placeholder="Gamer Tag" name="xname"></input>
+<input id="newUserWorld" type="text" placeholder="World Title" name="xword"></input>
+<input id="newUserPin" type="password" placeholder="4 digit pin" name="xpin"></input>
+<button id="newUserSubmit" type="Submit" name="submit" onmouseover="this.style.background = '#000000';this.style.color = '#ffffff';" onmouseout="this.style.background = '#4c4c4c';this.style.color = '#ffffff';">CREATE</button>
+</div>
 
 <img id="xmenu" src="img/menuBox.png" style="display:none;" onclick="hideGenes();hideMenu();">
 
--- usr/share/applications/ioniclib/js/index.js
+++ usr/share/applications/ioniclib/js/index.js
@@ -97,7 +97,9 @@ function hideMenu() {
 		document.getElementById('xmenu').style.display = 'none';
 		document.getElementById('titleText').style.opacity = '1.0';
 		document.getElementById('opt2Text').style.opacity = '1.0';
-		document.getElementById('opt3Text').style.opacity = '1.0';	
+		document.getElementById('opt3Text').style.opacity = '1.0';
+		resetSteps();
+		hideUserAtts();	
 }
 
 function closeApp() {
@@ -110,6 +112,12 @@ function showGenes() {
 	var geneAstat = document.getElementById('geneA').style.display;
 	var geneBstat = document.getElementById('geneB').style.display;
 	var geneCstat = document.getElementById('geneC').style.display;
+	var geneTextStat = document.getElementById('geneTitle').style.display;
+	var geneStepStat = document.getElementById('geneSteps').style.display;
+
+	document.getElementById('geneA').style.zIndex = '5';
+	document.getElementById('geneB').style.zIndex = '5';
+	document.getElementById('geneC').style.zIndex = '5';
 
 	if ( geneAstat = "none" ) {
 
@@ -137,6 +145,24 @@ function showGenes() {
 
 		document.getElementById('geneC').style.display = "none";
 	}
+
+	if ( geneTextStat = "none" ) {
+
+		document.getElementById('geneTitle').style.display = "block";
+
+	} else {
+
+		document.getElementById('geneTitle').style.display = "none";
+	}
+
+	if ( geneStepStat = "none" ) {
+
+		document.getElementById('geneSteps').style.display = "block";
+
+	} else {
+
+		document.getElementById('geneSteps').style.display = "none";
+	}
 }
 
 function hideGenes() {
@@ -144,6 +170,8 @@ function hideGenes() {
 	var geneAstat = document.getElementById('geneA').style.display;
 	var geneBstat = document.getElementById('geneB').style.display;
 	var geneCstat = document.getElementById('geneC').style.display;
+	var geneTextStat = document.getElementById('geneTitle').style.display;
+	var geneStepStat = document.getElementById('geneSteps').style.display;
 
 	if ( geneAstat = "block" ) {
 
@@ -171,6 +199,239 @@ function hideGenes() {
 
 		document.getElementById('geneC').style.display = "block";
 	}
+
+	if ( geneTextStat = "block" ) {
+
+		document.getElementById('geneTitle').style.display = "none";
+
+	} else {
+
+		document.getElementById('geneTitle').style.display = "block";
+	}
+
+	if ( geneStepStat = "block" ) {
+
+		document.getElementById('geneSteps').style.display = "none";
+
+	} else {
+
+		document.getElementById('geneSteps').style.display = "block";
+	}
+}
+
+function stepTwoGeneA() {
+
+	var geneTextStat = document.getElementById('geneTitle').style.display;
+	var geneStepStat = document.getElementById('geneSteps').style.display;
+
+	document.getElementById('geneA').style.zIndex = '1';
+
+	if ( geneTextStat = "none" ) {
+
+		document.getElementById('geneTitle').style.display = "block";
+		document.getElementById('femaleOpt').style.display = "block";
+		document.getElementById('maleOpt').style.display = "block";
+
+	} else {
+
+		document.getElementById('geneTitle').style.display = "none";
+		document.getElementById('femaleOpt').style.display = "none";
+		document.getElementById('maleOpt').style.display = "none";
+	}
+
+	if ( geneStepStat = "none" ) {
+
+		document.getElementById('geneSteps').style.display = "block";
+		document.getElementById('femaleOpt').style.display = "block";
+		document.getElementById('maleOpt').style.display = "block";
+
+	} else {
+
+		document.getElementById('geneSteps').style.display = "none";
+		document.getElementById('femaleOpt').style.display = "none";
+		document.getElementById('maleOpt').style.display = "none";
+	}
+
+	document.getElementById('geneTitle').innerHTML = 'The Poseidon Gene';
+	document.getElementById('geneSteps').innerHTML = 'Profile Setup 2 of 3, Select a Gender..';
+	document.getElementById('geneA').style.display = 'none';
+	document.getElementById('geneB').style.display = 'none';
+	document.getElementById('geneC').style.display = 'none';
+}
+
+function stepTwoGeneB() {
+
+	var geneTextStat = document.getElementById('geneTitle').style.display;
+	var geneStepStat = document.getElementById('geneSteps').style.display;
+
+	if ( geneTextStat = "none" ) {
+
+		document.getElementById('geneTitle').style.display = "block";
+		document.getElementById('femaleOpt').style.display = "block";
+		document.getElementById('maleOpt').style.display = "block";
+
+	} else {
+
+		document.getElementById('geneTitle').style.display = "none";
+		document.getElementById('femaleOpt').style.display = "none";
+		document.getElementById('maleOpt').style.display = "none";
+	}
+
+	if ( geneStepStat = "none" ) {
+
+		document.getElementById('geneSteps').style.display = "block";
+		document.getElementById('femaleOpt').style.display = "block";
+		document.getElementById('maleOpt').style.display = "block";
+
+	} else {
+
+		document.getElementById('geneSteps').style.display = "none";
+		document.getElementById('femaleOpt').style.display = "none";
+		document.getElementById('maleOpt').style.display = "none";
+	}
+
+	document.getElementById('geneTitle').innerHTML = 'The Gaia Gene';
+	document.getElementById('geneSteps').innerHTML = 'Profile Setup 2 of 3, Select a Gender..';
+	document.getElementById('geneA').style.display = 'none';
+	document.getElementById('geneB').style.display = 'none';
+	document.getElementById('geneC').style.display = 'none';
+}
+
+function stepTwoGeneC() {
+
+	var geneTextStat = document.getElementById('geneTitle').style.display;
+	var geneStepStat = document.getElementById('geneSteps').style.display;
+
+	if ( geneTextStat = "none" ) {
+
+		document.getElementById('geneTitle').style.display = "block";
+		document.getElementById('femaleOpt').style.display = "block";
+		document.getElementById('maleOpt').style.display = "block";
+
+	} else {
+
+		document.getElementById('geneTitle').style.display = "none";
+		document.getElementById('femaleOpt').style.display = "none";
+		document.getElementById('maleOpt').style.display = "none";
+	}
+
+	if ( geneStepStat = "none" ) {
+
+		document.getElementById('geneSteps').style.display = "block";
+		document.getElementById('femaleOpt').style.display = "block";
+		document.getElementById('maleOpt').style.display = "block";
+
+	} else {
+
+		document.getElementById('geneSteps').style.display = "none";
+		document.getElementById('femaleOpt').style.display = "none";
+		document.getElementById('maleOpt').style.display = "none";
+	}
+
+	document.getElementById('geneTitle').innerHTML = 'The Hedes Gene';
+	document.getElementById('geneSteps').innerHTML = 'Profile Setup 2 of 3, Select a Gender..';
+	document.getElementById('geneA').style.display = 'none';
+	document.getElementById('geneB').style.display = 'none';
+	document.getElementById('geneC').style.display = 'none';
+}
+
+function resetSteps() {
+
+	document.getElementById('geneTitle').innerHTML = 'Select Your Family Gene';
+	document.getElementById('geneSteps').innerHTML = 'Profile Setup 1 of 3';
+	document.getElementById('femaleOpt').style.display = "none";
+	document.getElementById('maleOpt').style.display = "none";
+	showAllGenes();
+}
+
+function HideGeneA () {
+	
+	var aStat = document.getElementById('geneA').style.display;
+
+	if ( aStat = "none" ) {
+
+		document.getElementById('geneA').style.display = 'block';
+
+	} else {
+
+		document.getElementById('geneA').style.display = 'none';
+	}
+}
+
+function HideGeneB () {
+	
+	var bStat = document.getElementById('geneB').style.display;
+
+	if ( bStat = "none" ) {
+
+		document.getElementById('geneB').style.display = 'block';
+
+	} else {
+
+		document.getElementById('geneB').style.display = 'none';
+
+	}
+}
+
+function HideGeneC () {
+	
+	var cStat = document.getElementById('geneC').style.display;
+
+	if ( cStat = "none" ) {
+
+		document.getElementById('geneC').style.display = 'block';
+
+	} else {
+
+		document.getElementById('geneC').style.display = 'none';
+
+	}
+}
+
+function icoGeneA () {
+	
+	document.getElementById('geneA').style = 'display:none';
+
+}
+
+function icoGeneB () {
+	
+	document.getElementById('geneB').style = 'display:none';
+}
+
+function icoGeneC () {
+	
+	document.getElementById('geneC').style = 'display:none';
+}
+
+function hideAllGenes() {
+
+	document.getElementById('genePool').style.display = 'none';
+	document.getElementById('geneXY').style.display = 'block';
+}
+
+function showAllGenes() {
+
+	document.getElementById('genePool').style.display = 'block';
+}
+
+function openUserAttsMale() {
+
+	document.getElementById('geneSteps').innerHTML = 'Profile Setup 3 of 3, Player Details..';
+	document.getElementById('geneXY').style.display = "none";
+	document.getElementById('userAtts').style.display = 'block';
+}
+
+function openUserAttsFemale() {
+
+	document.getElementById('geneSteps').innerHTML = 'Profile Setup 3 of 3, Player Details..';
+	document.getElementById('geneXY').style.display = "none";
+	document.getElementById('userAtts').style.display = 'block';
+}
+
+function hideUserAtts() {
+
+	document.getElementById('userAtts').style.display = 'none';
 }
 
 
